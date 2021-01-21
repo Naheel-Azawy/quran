da4f0f92615d160010ed374e927024975a20d96c
Naheel-Azawy <naheelazawy@gmail.com>
2021-01-21 22:35:11 +0300

    terminal padding right

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ':' //; N=$(command -v node || command -v nodejs)
 ':' //; [ "$1" = -n ] && exec "$N" "$0" "$@"
-':' //; W=$(tput cols)
+':' //; W=$(tput cols); W=$((W-1))
 ':' //; "$N" "$0" -w "$W" "$@" | fribidi --nobreak -w "$W" | less -F
 ':' //; exit
 
